82190d56673e3b6a63f2d8103c357430751ab29b
Rishala Itshnyk <rishala.itshnyk@gmail.com>
2023-12-07 16:45:36 +0200

    initial commit

---
+++ Laboratorna_6_1_REC.Tests/Tests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Laboratorna_6_1_REC;
+using NUnit.Framework;
+
+namespace Laboratorna_6_1_ITER_Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void TestFillArray()
+        {
+            int[] b = new int[21];
+            Program.FillArray(b);
+
+            for (int i = 0; i < 21; ++i)
+            {
+                Assert.IsTrue(b[i] >= 10 && b[i] <= 90);
+            }
+        }   
+
+        [Test]
+        public void TestProcessArray()
+        {
+            int[] b = { 12, 15, 18, 21, 24, 27, 30, 33, 36, 39 };
+            int count, sum;
+
+            Program.ProcessArray(b, out count, out sum);
+
+            Assert.AreEqual(5, count);
+            Assert.AreEqual(120, sum);
+
+            for (int i = 0; i < b.Length; ++i)
+            {
+                if (!(b[i] % 2 != 0 && b[i] % 3 == 0))
+                {
+                    Assert.AreEqual(0, b[i]);
+                }
+            }
+        }
+    }
+}
---
+++ Laboratorna_6_1_REC/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Laboratorna_6_1_REC
+{
+    public class Program
+    {
+        public static void FillArray(int[] b, int index = 0)
+        {
+            Random random = new Random();
+            if (index < 21)
+            {
+                b[index] = random.Next(10, 91);
+                FillArray(b, index + 1);
+            }
+        }
+
+        public static void PrintArray(int[] b, int index = 0)
+        {
+            if (index < 21)
+            {
+                Console.Write($"{b[index],4}");
+                PrintArray(b, index + 1);
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+        
+        public static void ProcessArrayRecursive(int[] b, int index, ref int count, ref int sum)
+        {
+            if (index < b.Length)
+            {
+                if (!(b[index] % 2 != 0 && b[index] % 3 == 0))
+                {
+                    count++;
+                    sum += b[index];
+                    b[index] = 0;
+                }
+                ProcessArrayRecursive(b, index + 1, ref count, ref sum);
+            }
+        }
+        
+        public static void ProcessArray(int[] arr, out int count, out int sum)
+        {
+            count = 0;
+            sum = 0;
+            ProcessArrayRecursive(arr, 0, ref count, ref sum);
+        }
+        
+        public static void Main()
+        {
+            int[] myArray = new int[21];
+            int count, sum;
+
+            FillArray(myArray);
+            Console.Write("Original Array: ");
+            PrintArray(myArray);
+
+            ProcessArray(myArray, out count, out sum);
+            Console.Write("Modified Array: ");
+            PrintArray(myArray);
+            Console.WriteLine($"Count of elements satisfying the criteria: {count}");
+            Console.WriteLine($"Sum of elements satisfying the criteria: {sum}");
+
+             Console.ReadLine();
+        }
+    }
+}
